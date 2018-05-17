08dc1f5aaa5ba7a9d6cb21fa7fa69583d8cdb1d4
Thiago <thiagopuntar@gmail.com>
2018-05-17 16:57:08 -0300

    inicial

--- public/script.js
+++ public/script.js
@@ -1,1 +1,27 @@
-console.log('It works.');
+new Vue({
+    el: "#app",
+    data: {
+        items: [
+            {id: 1, title: 'Item1'},
+            {id: 2, title: 'Item2'},
+            {id: 3, title: 'Item3'}
+        ],
+        cart:[]
+    },
+    methods: {
+        addToCart(index) {
+            let item = this.items[index];
+            let itemCart = this.cart.find((x => x.id == item.id))
+            
+            if (itemCart){
+                itemCart.qty++;
+            } else {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1
+                })
+            }
+        }
+    }
+})
