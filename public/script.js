18d8d858de4bf3d6e360f8ba75cdcc18606831ac
Thiago <thiagopuntar@gmail.com>
2018-05-21 14:39:29 -0300

    atualizações

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
+const PRICE = 9.99;
+
 new Vue({
     el: "#app",
     data: {
+        total: 0,
         items: [
             {id: 1, title: 'Item1'},
             {id: 2, title: 'Item2'},
@@ -8,10 +11,13 @@ new Vue({
         ],
         cart:[]
     },
+    filters: {
+        currency: function(item){
+            return `$ ${item.toFixed(2)}`;
+        }
+    },
     methods: {
         addToCart(index) {
-            let teste;
-
             let item = this.items[index];
             let itemCart = this.cart.find((x => x.id == item.id))
             
@@ -21,8 +27,22 @@ new Vue({
                 this.cart.push({
                     id: item.id,
                     title: item.title,
-                    qty: 1
+                    qty: 1,
+                    price: PRICE
                 })
+            }
+
+            this.total += PRICE;
+        },
+        inc(index){
+            this.cart[index].qty++;
+            this.total += PRICE;
+        },
+        dec(index){
+            this.cart[index].qty--;
+            this.total -= PRICE;
+            if (this.cart[index].qty == 0) {
+                this.cart.splice(index, 1);
             }
         }
     }
